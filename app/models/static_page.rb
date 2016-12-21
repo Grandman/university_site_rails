2eee765130dc3bbf160293f27b9c0b9feff364d3
Grandman <root@grandman73.ru>
2016-12-21 21:35:55 +0400

    add format to slug

--- app/models/static_page.rb
+++ app/models/static_page.rb
@@ -1,5 +1,5 @@
 class StaticPage < ActiveRecord::Base
-  validates :slug, uniqueness: true, presence: true
+  validates :slug, uniqueness: true, presence: true, format: { with: %r{([aA-zZ0-9_-]+/*)+}, message: 'неправильный формат' }
   validates :name, presence: true
   validates :content, presence: true
 
