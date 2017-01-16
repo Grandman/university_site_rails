152516ca2d9b24793c63e5bcbdd0592227c0f4bf
Grandman <root@grandman73.ru>
2017-01-16 22:49:45 +0400

    dependent destroy for user news

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,7 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-  has_many :news
+  has_many :news, dependent: :destroy
   has_many :disciplines
 
   enumerize :role, in: [:student, :admin, :teacher], default: :student, predicates: true
